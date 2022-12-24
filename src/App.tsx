085cc2b4f4d0560c900e5f7f9aebc1c1c8d243b8
saram <yujun1018@naver.com>
2022-12-24 14:15:24 +0900

    design: GlobalStyle reset 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import GlobalStyles from 'components/GlobalStyle';
 import Intro from 'pages/Intro/Intro';
 
 function App() {
   return (
     <div className='App'>
+      <GlobalStyles />
       <Intro />
     </div>
   );
