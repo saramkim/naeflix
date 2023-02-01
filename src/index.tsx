0ae1064a3501dfb8a49725ade66af83c58da3154
saram <yujun1018@naver.com>
2023-02-02 00:39:59 +0900

    chore: gh-pages 배포 설정

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <GlobalStyles />
       <App />
     </BrowserRouter>
