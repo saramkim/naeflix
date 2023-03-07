92986261c7f7e23a41d66cc6d6aab5a68944035d
saram <yujun1018@naver.com>
2023-03-07 14:36:43 +0900

    feat: introduction 아무 영화 추가

--- src/pages/introduction/ImageTextBundle.tsx
+++ src/pages/introduction/ImageTextBundle.tsx
@@ -1,4 +1,5 @@
 import account from 'assets/account.png';
+import anyMovie from 'assets/any-movie.png';
 import bestCinema from 'assets/best-cinema.mp4';
 import home from 'assets/home.png';
 import intro from 'assets/intro.png';
@@ -27,6 +28,9 @@ function ImageTextBundle() {
         평점별 영화 목록, 인기 영화 목록 등 다양한 영화 목록을 확인할 수 있습니다.
         <br />
         <br />
+        시청한 영화, 자신의 평점별 영화 목록을 통해 나만의 영화 컬렉션을 만들 수 있습니다.
+        <br />
+        <br />
         사용자가 직접 홈 화면 목록을 설정 가능합니다.
       </ImageTextBox>
       <ImageTextBox title='영화 상세 화면' img={movieDetail}>
@@ -44,7 +48,13 @@ function ImageTextBundle() {
       <ImageTextBox title='검색 기능' img={search} direction='reverse'>
         제목으로 영화를 검색하고, 이름으로 인물을 검색할 수 있습니다.
       </ImageTextBox>
-      <ImageTextBox title='계정 화면' img={account}>
+      <ImageTextBox title='아무 영화' img={anyMovie}>
+        장르와 나라 등을 선택하여 해당하는 영화들 중에서 무작위로 아무 영화를 소개합니다.
+        <br />
+        <br />
+        대중적으로 널리 알려지지 않은 영화를 발굴하고, 영화를 추천 받을 수 있습니다.
+      </ImageTextBox>
+      <ImageTextBox title='계정 화면' img={account} direction='reverse'>
         계정 정보와 프로필을 입력하고 변경할 수 있습니다.
         <br />
         <br /> 구글 계정과 연동 가능합니다.
